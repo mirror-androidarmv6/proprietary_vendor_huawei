ec9855be67a3be1d4bf40e893c3b853ede3d4777
Jurica Vukadin <u.ra604@gmail.com>
2015-11-11 13:21:06 +0100

    mt2: Update Adreno blobs
    * from Adreno-Nexus5-MRA58K

--- mt2/mt2-vendor-blobs.mk
+++ mt2/mt2-vendor-blobs.mk
@@ -171,12 +171,14 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/mt2/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
     vendor/huawei/mt2/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:system/vendor/lib/egl/libGLESv1_CM_adreno.so \
     vendor/huawei/mt2/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
+    vendor/huawei/mt2/proprietary/vendor/lib/egl/libplayback_adreno.so:system/vendor/lib/egl/libplayback_adreno.so \
     vendor/huawei/mt2/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libRSDriver_adreno.so:system/vendor/lib/libRSDriver_adreno.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/huawei/mt2/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
     vendor/huawei/mt2/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
